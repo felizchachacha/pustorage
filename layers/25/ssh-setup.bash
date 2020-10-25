b151e395f98efdf983fb5c766826c7d119a34c6c
Your Name <you@example.com>
2020-10-25 21:52:42 +0000

    fixes

--- layers/25/ssh-setup.bash
+++ layers/25/ssh-setup.bash
@@ -5,18 +5,17 @@ set -xe
 readonly MYDIR=$(dirname $(realpath ${0}))
 readonly DEFUSER="$(cat ${MYDIR}/../../lib/DEFUSER)"
 
-
 pushd ${MYDIR}
 
 	cp -rv --backup ssh-setup/* /
 
 popd
 pushd "${MYDIR}"/../..
-	exe/ensure_path.bash ~root/.ssh ~"${DEFUSER}"/.ssh
-	chmod 0700 ~root/.ssh ~"${DEFUSER}"/.ssh
-	cat ./lib/PUB | tee ~/.ssh/authorized_keys > ~"${DEFUSER}"/.ssh/authorized_keys
-	chmod 0600 ~/.ssh/authorized_keys ~"${DEFUSER}"/.ssh/authorized_keys
-	chown -R "${DEFUSER}":"${DEFUSER}" ~"${DEFUSER}"/.ssh
+	exe/ensure_path.bash /root/.ssh /home/"${DEFUSER}"/.ssh
+	chmod 0700 /root/.ssh /home/"${DEFUSER}"/.ssh
+	cat ./lib/PUB | tee /root/.ssh/authorized_keys > /home/"${DEFUSER}"/.ssh/authorized_keys
+	chmod 0600 /root/.ssh/authorized_keys /home/"${DEFUSER}"/.ssh/authorized_keys
+	chown -R "${DEFUSER}":"${DEFUSER}" /home/"${DEFUSER}"/.ssh
 
 	systemctl restart ssh &
 popd
