f0e99830836f318e9ebfc0e8dd948001dc0c92f3
Your Name <you@example.com>
2020-10-25 16:05:30 +0000

    fixes

--- exe/grub-probe.wrapper.bash
+++ exe/grub-probe.wrapper.bash
@@ -7,6 +7,8 @@ readonly ORIGPLACE=/usr/sbin/grub-probe.orig
 # grub-probe /
 # grub-probe: error: failed to get canonical path of `/cow'.
 
+declare -a Params=()
+
 function install() {
 	set -x
 	readonly THEPATH="/usr/sbin/grub-probe"
@@ -14,10 +16,23 @@ function install() {
 	diff -dq "${THEPATH}" "${0}" || cp --backup "${THEPATH}" "${ORIGPLACE}" && cp --backup "${ME}" "${THEPATH}"
 }
 
-if [[ "${1}" == "/" ]]; then
-	echo "zfs"
-elif [[ "${1}" == "install" ]]; then
-	install
-else
-	"${ORIGPLACE}" ${*}
-fi
+while [ ${#} -gt 0 ]; do
+	case ${1} in
+		'install')
+			install
+			exit 0
+		;;
+		'/')
+			echo "zfs"
+			exit 0
+		;;
+		*)
+			Params+="${1}"
+                        shift # past param
+		;;
+	esac
+done
+
+
+"${ORIGPLACE}" ${Params[@]}
+
