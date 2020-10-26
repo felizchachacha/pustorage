3ff3b9d68edb4cc86a608e73abd091d7f2befc3f
Your Name <you@example.com>
2020-10-26 04:52:39 +0400

    ease

--- layers/75/homebrew.bash
+++ layers/75/homebrew.bash
@@ -5,16 +5,14 @@ set -xe
 readonly MYDIR="$(dirname $(realpath ${0}))"
 
 readonly DWNLD_LINK="https://raw.githubusercontent.com/Homebrew/install/master/install.sh"
-readonly TARGET_FNAME="homebrew_install.sh"
+readonly TARGET_F="/tmp/homebrew_install.sh"
 
 pushd "${MYDIR}"
-	wget -c "${DWNLD_LINK}" -O "${TARGET_FNAME}"
-	chmod +x "${TARGET_FNAME}"
-	./"${TARGET_FNAME}" || if [[ $(./"${TARGET_FNAME}") == "Don't run this as root!" ]]; then
+	wget -c "${DWNLD_LINK}" -O "${TARGET_F}"
+	chmod +x "${TARGET_F}"
+	"${TARGET_F}" || if [[ $("${TARGET_F}") == "Don't run this as root!" ]]; then
 		readonly U="$(cat ../../lib/DEFUSER)"
-		#ln -vb "${TARGET_FNAME}" /tmp/"${TARGET_FNAME}" || cp -b "${TARGET_FNAME}" /tmp/"${TARGET_FNAME}"
-		mv -v "${TARGET_FNAME}" /tmp/"${TARGET_FNAME}"
-		chown "${U}":"${U}" /tmp/"${TARGET_FNAME}"
-		su - "${U}" -c /tmp/"${TARGET_FNAME}"
+		chown "${U}":"${U}" "${TARGET_FNAME}"
+		su - "${U}" -c "${TARGET_FNAME}"
 	fi
 popd
