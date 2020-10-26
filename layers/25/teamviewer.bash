682f7f1e803353a94ecae08ec07ed39e4ba4a12f
Your Name <you@example.com>
2020-10-26 16:41:30 +0000

    home
    fixes

--- layers/25/teamviewer.bash
+++ layers/25/teamviewer.bash
@@ -1,21 +1,26 @@
 #!/usr/bin/env bash
 
-set -ex
+set -e
 
 readonly MYDIR=$(dirname ${0})
+readonly DEFUSER="$(cat ${MYDIR}/../../lib/DEFUSER)"
 
 teamviewer daemon enable
-#teamviewer daemon status # interactive
 teamviewer repo
-read -e -p "Enter teamviewer temporary password: " TP
-teamviewer passwd ${TP}
+teamviewer daemon start
 teamviewer info
 readonly TVID=$(teamviewer info | awk '/TeamViewer ID/ {print $NF}')
-teamviewer daemon start
 teamviewer daemon status
-echo -e "Now you can try:
-ID:\t${TVID}
-with a temp password:\t${TP}
-"
 
-[[ "${TVID}" == *[^[:space:]]* ]] && "${MYDIR}"/../../exe/sux.sh $(cat "${MYDIR}"/../../lib/DEFUSER) -c teamviewer
+if (( TVID > 0 )); then
+	read -e -p "Enter teamviewer temporary password (at least 8 characters): " TP
+	teamviewer passwd ${TP}
+	teamviewer daemon restart
+	echo -e "Now you can try:
+	ID:\t${TVID}
+	with a temp password:\t${TP}
+	"
+else
+	su - "${DEFUSER}" -c "DISPLAY=:0.0 teamviewer" &
+fi
+
