f1342872a91bcbe10e4a067f3757db94bba3b7c0
Your Name <you@example.com>
2020-10-26 04:50:00 +0400

    fixes

--- layers/75/fullupgrade.bash
+++ layers/75/fullupgrade.bash
@@ -6,7 +6,7 @@ readonly MYDIR=$(dirname ${0})
 
 pushd ${MYDIR}
 
-	packages/aptilists.bash full-upgrade extra.list basic.list withgui.list minimal.list custom.list
+	../../exe/aptilists.bash full-upgrade basic.list withgui.list minimal.list custom.list
 
 	flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 
