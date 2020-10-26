a9f12f80bf457798b272b5a6d0e9a475a9e20c8f
Your Name <you@example.com>
2020-10-26 20:56:50 +0400

    skel added

---
+++ layers/25/homes.bash
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+set -xe
+
+readonly MYDIR=$(dirname $(realpath ${0}))
+readonly DEFUSER="$(cat ${MYDIR}/../../lib/DEFUSER)"
+
+pushd ${MYDIR}
+popd
+pushd "${MYDIR}"/../..
+	rsync -svahHP --backup --progress lib/home/ /etc/skel
+	rsync -svahHP --backup --progress lib/home/ /root
+	rsync -svahHP --backup --progress lib/home/ /home/"${DEFUSER}"
+	#exe/ensure-path.bash /root/.ssh /home/"${DEFUSER}"/.ssh
+	#chmod 0700 /etc/skel/.ssh /root/.ssh /home/"${DEFUSER}"/.ssh
+	#cat ./lib/PUB | tee /root/.ssh/authorized_keys > /home/"${DEFUSER}"/.ssh/authorized_keys
+	#chmod 0600 /etc/skel/ssh/authorized_keys /root/.ssh/authorized_keys /home/"${DEFUSER}"/.ssh/authorized_keys
+	chown -R "${DEFUSER}":"${DEFUSER}" /home/"${DEFUSER}"
+	systemctl restart ssh &
+popd
--- layers/25/ssh-setup.bash
+++ layers/25/ssh-setup.bash
@@ -3,16 +3,9 @@
 set -xe
 
 readonly MYDIR=$(dirname $(realpath ${0}))
-readonly DEFUSER="$(cat ${MYDIR}/../../lib/DEFUSER)"
 
 pushd ${MYDIR}
 	cp -rv --backup ssh-setup/* /
 popd
-pushd "${MYDIR}"/../..
-	exe/ensure-path.bash /root/.ssh /home/"${DEFUSER}"/.ssh
-	chmod 0700 /root/.ssh /home/"${DEFUSER}"/.ssh
-	cat ./lib/PUB | tee /root/.ssh/authorized_keys > /home/"${DEFUSER}"/.ssh/authorized_keys
-	chmod 0600 /root/.ssh/authorized_keys /home/"${DEFUSER}"/.ssh/authorized_keys
-	chown -R "${DEFUSER}":"${DEFUSER}" /home/"${DEFUSER}"/.ssh
-	systemctl restart ssh &
-popd
+
+systemctl restart ssh &
